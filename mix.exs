8417f3ade7d0640a433955ef848b9766209be8d7
t-ooe <supergrass0@gmail.com>
2015-08-26 19:05:17 +0900

    api呼び出せるか試した

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Kanzashi.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:httpoison, "~> 0.6"}]
   end
 end
--- web/controllers/items_controller.ex
+++ web/controllers/items_controller.ex
@@ -4,8 +4,22 @@ defmodule Kanzashi.ItemsController do
   plug :action
 
   def list(conn, _params) do
-    conn
-    |> put_status(200)
-    |> json  %{ok: true}
+    HTTPoison.start
+
+    url = "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222?format=json&keyword=%E3%81%8B%E3%82%8F%E3%81%84%E3%81%84&applicationId=1067816662614119417"
+    case HTTPoison.get(url) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        js = Poison.Parser.parse!(body, keys: :atoms!)
+        conn
+        |> put_status(200)
+        |> json  %{ok: true, result: js}
+      {:ok, %HTTPoison.Response{status_code: 404}} ->
+        IO.puts "Not found :("
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+    # conn
+    # |> put_status(200)
+    # |> json  %{ok: true}
   end
 end
